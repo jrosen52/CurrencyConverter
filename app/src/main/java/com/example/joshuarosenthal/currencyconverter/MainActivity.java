78441ee62ec54d015c5a1c6394630e38762eae7c
Joshua Rosenthal <jrosen52@binghamton.edu>
2019-09-05 18:39:57 -0400

    Added spinner, text, and button

--- app/src/main/java/com/example/joshuarosenthal/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/joshuarosenthal/currencyconverter/MainActivity.java
@@ -9,16 +9,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] currency={"Dollar","Cad","Euro","Peso","Yen"};
-    int images[] = {R.drawable.dollar,R.drawable.cad, R.drawable.euro, R.drawable.peso, R.drawable.yen };
+    String[] currency={"Pound","Cad","Euro","Peso","Yuan"};
+    int images[] = {R.drawable.pound,R.drawable.cad, R.drawable.euro, R.drawable.peso, R.drawable.yen };
+    double euroV = .91;
+    double yuanV = 7.15;
+    double cadV = 1.33;
+    double poundV = .82;
+    double pesoV = 19.79;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spin = (Spinner) findViewById(R.id.spinner);
-        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        Spinner spin1 = (Spinner) findViewById(R.id.spinner1);
+        spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(MainActivity.this, "You Select Position: "+position+" "+currency[position], Toast.LENGTH_SHORT).show();
@@ -30,8 +35,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        SpinnerAdapter customAdapter = new SpinnerAdapter(getApplicationContext(),images,currency);
-        spin.setAdapter(customAdapter);
+        SpinnerAdapter customAdapter1 = new SpinnerAdapter(getApplicationContext(),images,currency);
+        spin1.setAdapter(customAdapter1);
+
+
+        Spinner spin2 = (Spinner) findViewById(R.id.spinner2);
+        spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Two Select Position: "+position+" "+currency[position], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        SpinnerAdapter customAdapter2 = new SpinnerAdapter(getApplicationContext(),images,currency);
+        spin2.setAdapter(customAdapter2);
 
     }
 }
